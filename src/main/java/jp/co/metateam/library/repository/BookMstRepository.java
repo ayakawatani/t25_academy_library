3f220d5d47a684b77d78c014aea04257a6517f17
ayakawatani <aya.kawatani@metateam.co.jp>
2025-05-16 11:46:13 +0900

    重複チェックの修正

--- src/main/java/jp/co/metateam/library/repository/BookMstRepository.java
+++ src/main/java/jp/co/metateam/library/repository/BookMstRepository.java
@@ -15,7 +15,7 @@ public interface BookMstRepository extends JpaRepository<BookMst, Long> {
 	@Query(value = "SELECT * FROM book_mst WHERE id = ?1", nativeQuery = true)
 	Optional<BookMst> selectById(Long id);
 
-	@Query(value = "SELECT * FROM book_mst b WHERE b.isbn = ?1", nativeQuery = true)
+	@Query(value = "SELECT * FROM book_mst b WHERE b.isbn = ?1 AND b.deleted = false", nativeQuery = true)
 	List<BookMst> selectByIsbn(String isbn);
 
 	@Query("SELECT b FROM BookMst b WHERE b.deleted = false")
